252179a9a6217cb424978ea1a5291c10b3547934
ahmedsmrt <ahmedsmrt@gmail.com>
2022-07-31 23:02:40 -0400

    Notes With Nuance Version Two Phase one

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+import UserForm from "./components/UserForm";
+import Welcome from "./components/Welcome";
+
+const MainTheme = styled.main`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 function App() {
+  const [welcomeText, setWelcomeText] = useState(true)
+  const [userFormState, setUserFormState] = useState("")
+
+  const handleClick = (e) => {
+    let userFormClicked = e.target.value;
+    setWelcomeText(false)
+    setUserFormState(userFormClicked)
+    console.log(userFormClicked)
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainTheme>
+      {welcomeText ? <Welcome handleClick={handleClick} /> : <UserForm  formState={userFormState} />}
+    </MainTheme>
   );
 }
 
